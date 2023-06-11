12d72d9f9a566421d47e46a2766355c7e1f153ec
swatibersurda <swatibersurda@gmail.com>
2023-06-11 20:06:16 +0530

    added_header

--- tablemanipulation-datavis/src/Components/AllRoutes.jsx
+++ tablemanipulation-datavis/src/Components/AllRoutes.jsx
@@ -5,7 +5,7 @@ import Prac from "./Prac";
 export const AllRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>}></Route>
+      <Route path="/" element={<Prac/>}></Route>
     </Routes>
   );
 };
--- tablemanipulation-datavis/src/Components/Prac.jsx
+++ tablemanipulation-datavis/src/Components/Prac.jsx
@@ -1,52 +1,59 @@
 import React from "react";
 import "./home.css";
-import {FaTrashAlt} from "react-icons/fa"
-import {IoFilterSharp} from "react-icons/io5"
+import { FaTrashAlt } from "react-icons/fa";
+import { IoFilterSharp } from "react-icons/io5";
+import { AiOutlineSearch } from "react-icons/ai";
+import { RiDeleteBinLine } from "react-icons/ri";
+import { BsFilter } from "react-icons/bs";
+import { TbFileExport } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
 const Prac = () => {
   return (
     <div className="cover">
       <div className="tablee">
-        <div className="cardheader">
-            <div className="content1">
-                <div className="Textandsupportingtext"></div>
-                <div className="actionss">
-                    <div className="actionChild">
-                        <div className="buttonbase">
-                            <div className="trash">
-                            <FaTrashAlt className="tra"></FaTrashAlt>
-                           
-                            </div>
-                            <div className="deltext">
-                                Delete
-                            </div>
-                        </div>
-                        <div className="buttonbase">
-                            <div className="trash">
-                            <IoFilterSharp className="tra"></IoFilterSharp>
-                           
-                            </div>
-                            <div className="deltext">
-                                Filters
-                            </div>
-                        </div>
-                        <div className="buttonbase">
-                            <div className="trash">
-                            <IoFilterSharp className="tra"></IoFilterSharp>
-                           
-                            </div>
-                            <div className="deltext">
-                                Filters
-                            </div>
-                        </div>
-                        
-                    </div>
+        <div className="cardHeader">
+          <div className="contentDiv">
+            <div className="textSupportDiv">
+              <h4 className="headingDiv">Heading</h4>
+              <div className="labelForText">Label for value</div>
+              <div className="inputDiv">
+                <div className="iconDiv">
+                  <AiOutlineSearch></AiOutlineSearch>
                 </div>
+                <div className="serchTextDiv">Search</div>
+                <input className="inpu" />
+              </div>
             </div>
-            
+            <div className="actionss">
+              <div className="del">
+                <div className="delIconDiv">
+                  <RiDeleteBinLine className="delIconsDiv"></RiDeleteBinLine>
+                </div>
+                <div className="textDeleteDiv">Delete</div>
+              </div>
+              <div className="filter">
+                <div className="delIconDiv">
+                  <BsFilter className="delIconsDiv"></BsFilter>
+                </div>
+                <div className="textDeleteDiv">Filter</div>
+              </div>
+              {/* export div */}
+              <div className="exportDiv">
+                <div className="delIconDiv">
+                  <TbFileExport className="delIconsDiv"></TbFileExport>
+                </div>
+                <div className="textDeleteDiv">Export</div>
+              </div>
+              <div className="buttonDivAddNewCta">
+                <div className="delIconDiv">
+                  <AiOutlinePlus color="white" className="delIconsDiv"></AiOutlinePlus>
+                </div>
+                <div className="addctaDiv">Add new CTA</div>
+              </div>
+            </div>
+          </div>
         </div>
-        {/* cardheader or row colums */}
-        <div className="content2"></div>
-       </div>
+      </div>
     </div>
   );
 };
